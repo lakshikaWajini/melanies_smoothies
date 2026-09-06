801bdc9f17c62213fad55c7f1551d7d7b52f629a
B.L.A.W. Lakshika <150930874+lakshikaWajini@users.noreply.github.com>
2026-09-06 11:14:22 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -65,4 +65,4 @@ except Exception as e:
 #new section to  display smoothiefroot nutrition information
 import requests  
 smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
